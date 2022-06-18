3e0e669009557486ad2603d68a7a6d41916e2287
Jejkukany <young256daniel@gmail.com>
2022-06-18 22:47:42 +0200

    config cleanup

--- Config.py
+++ Config.py
@@ -2,13 +2,12 @@
 SOURCE = 1
 
 #Enter your ATEM switcher IP
-SWITCHERIP = "192.168.1.200"
+SWITCHERIP = "192.168.1.0"
 
 #Edit the progam or leave blank
 PROGRAM = """
 Singing</br>
 Dancing</br>
 Programming</br>
-Reading<bt>
-ahoj
+Reading
 """
